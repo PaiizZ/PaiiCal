693ffefcacd13f966710b9fca64a71594acaa52f
paiizz <wanchanapon.t@ku.th>
2016-04-13 12:26:39 +0700

    init

---
+++ app/src/main/java/com/example/paiizz/paiical/models/Information.java
@@ -0,0 +1,10 @@
+package com.example.paiizz.paiical.models;
+
+/**
+ * Created by PaiizZ on 4/13/2016 AD.
+ */
+public class Information {
+    private String name ;
+    private String gender ;
+    private int age ;
+}
