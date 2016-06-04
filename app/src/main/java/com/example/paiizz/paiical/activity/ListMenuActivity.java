db4f5b326536836396f80107093b4efd386be435
gunhappy <gunhappy123@gmail.com>
2016-06-05 00:44:10 +0700

    help freind fix bug

--- app/src/main/java/com/example/paiizz/paiical/activity/ListMenuActivity.java
+++ app/src/main/java/com/example/paiizz/paiical/activity/ListMenuActivity.java
@@ -2,6 +2,8 @@ package com.example.paiizz.paiical.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.paiizz.paiical.R;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 public class ListMenuActivity extends AppCompatActivity {
     private int index_category;
     private MenuAdapter menuAdapter;
+    private RecyclerView recyclerView;
     private Data data;
 
     @Override
@@ -30,35 +33,48 @@ public class ListMenuActivity extends AppCompatActivity {
         switch (index_category) {
             case 1: {
                 menuAdapter = new MenuAdapter(data.getListThaifood());
+                break;
             }
             case 2: {
                 menuAdapter = new MenuAdapter(data.getListSinglefood());
+                break;
             }
             case 3: {
                 menuAdapter = new MenuAdapter(data.getListInterfood());
+                break;
             }
             case 4: {
                 menuAdapter = new MenuAdapter(data.getListSnack());
+                break;
             }
             case 5: {
                 menuAdapter = new MenuAdapter(data.getListDessert());
+                break;
             }
             case 6: {
                 menuAdapter = new MenuAdapter(data.getListDrink());
+                break;
             }
             case 7: {
                 menuAdapter = new MenuAdapter(data.getListFriut());
+                break;
             }
             case 8: {
                 menuAdapter = new MenuAdapter(data.getListRestaurant());
+                break;
             }
             default: {
                 menuAdapter = new MenuAdapter(data.getListEtc());
+                break;
             }
 
 
         }
-
+        //menuAdapter = new MenuAdapter(data.getListThaifood());
+        recyclerView =(RecyclerView)findViewById(R.id.menu_Recycler);
+        recyclerView.setAdapter(menuAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.i("kkk",menuAdapter.getItemCount()+" ");
 
     }
 
